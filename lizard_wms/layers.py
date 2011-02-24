7923671695d88cfc6393178f18e781afddd5b3a5
jackha <jackha@gmail.com>
2011-02-24 11:31:17 +0000

    added layers.py

---
+++ lizard_wms/layers.py
@@ -0,0 +1,24 @@
+from lizard_map.workspace import WorkspaceItemAdapter
+
+
+class AdapterWMS(WorkspaceItemAdapter):
+    """Dummy adapter. It satisfies the GUI.
+    Registerd as 'wms'.
+    """
+
+    def layer(self, layer_ids=None, request=None):
+        return [], {}
+
+    def symbol_url(self, identifier=None, start_date=None, end_date=None):
+        """
+        returns symbol
+        """
+        icon_style = {
+            'icon': 'polygon.png',
+            'mask': ('mask.png', ),
+            'color': (0, 1, 0, 0)}
+        return super(AdapterWMS, self).symbol_url(
+            identifier=identifier,
+            start_date=start_date,
+            end_date=end_date,
+            icon_style=icon_style)
