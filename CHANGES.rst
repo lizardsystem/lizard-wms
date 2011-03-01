be091c5165654dff99f26590debdf74fe8c7f05a
jackha <jackha@gmail.com>
2011-03-01 11:04:16 +0000

    added tests, updated docs, removed local checkouts

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,4 +5,6 @@ Changelog of lizard-wms
 0.1 (unreleased)
 ----------------
 
+- Added initial functionality.
+
 - Initial library skeleton created by nensskel.  [Jack Ha]
--- lizard_wms/tests.py
+++ lizard_wms/tests.py
@@ -1,9 +1,14 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
 
 from django.test import TestCase
+from django.test.client import Client
 
 
-class ExampleTest(TestCase):
+class IntegrationTest(TestCase):
+    fixtures = ['lizard_wms',]
 
-    def test_something(self):
-        self.assertEquals(1, 1)
+    def test_homepage(self):
+        c = Client()
+        url = '/'
+        response = c.get(url)
+        self.assertEquals(response.status_code, 200)
