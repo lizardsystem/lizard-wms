4015f59d4762b47589545312b83b07613f9c9b4d
Roland van Laar <roland@micite.net>
2012-05-14 10:52:23 +0200

    Changed admin layout WMSSource.
    Show domain opf the WMSSource url.

--- lizard_wms/admin.py
+++ lizard_wms/admin.py
@@ -1,6 +1,21 @@
+import urlparse
+
 from django.contrib import admin
 
 from lizard_wms.models import WMSConnection, WMSSource
 
-for model in (WMSConnection, WMSSource):
+
+def source_domain(obj):
+    """Return the domain of the WMSSource url."""
+    return ("%s" % urlparse.urlparse(obj.url).netloc)
+
+source_domain.short_description = 'Domein'
+
+
+class WMSSourceAdmin(admin.ModelAdmin):
+    list_display = ('name', source_domain)
+
+admin.site.register(WMSSource, WMSSourceAdmin)
+
+for model in (WMSConnection, ):
     admin.site.register(model)
