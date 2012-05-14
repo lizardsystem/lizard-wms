23c4717cbfa7c623ebcbe4a4fa3e2283cb6f7d98
Roland van Laar <roland@micite.net>
2012-05-14 11:04:20 +0200

    Nicer admin for WMSSource.

--- lizard_wms/admin.py
+++ lizard_wms/admin.py
@@ -13,7 +13,9 @@ source_domain.short_description = 'Domein'
 
 
 class WMSSourceAdmin(admin.ModelAdmin):
-    list_display = ('name', source_domain)
+    list_display = ('name',  source_domain, 'connection')
+    search_fields = ('name', 'url', 'category__name', 'connection__title')
+    list_filter = ('category', )
 
 admin.site.register(WMSSource, WMSSourceAdmin)
 
