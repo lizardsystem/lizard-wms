11888d132b0561e892fc152cf594ed21d897a8cf
Reinout van Rees <reinout@vanrees.org>
2012-06-12 16:13:00 +0200

    Added reload-the-wms-connection as an action instead of a management command.

--- lizard_wms/admin.py
+++ lizard_wms/admin.py
@@ -1,10 +1,14 @@
 """Configuration of the admin interface"""
 import urlparse
+import urllib2
+import logging
 
 from django.contrib import admin
 from django import forms
 from lizard_maptree.models import Category
 from lizard_wms import models
+
+logger = logging.getLogger(__name__)
 
 
 def source_domain(obj):
@@ -60,5 +64,29 @@ class WMSSourceAdmin(admin.ModelAdmin):
     form = WMSSourceForm
 
 
+class WMSConnectionAdmin(admin.ModelAdmin):
+
+    list_display = ('slug', 'title', 'url')
+    actions = ['reload']
+
+    def reload(self, request, queryset):
+        try:
+            for wms_connection in queryset:
+                wms_connection.fetch()
+        except AttributeError, e:
+            logger.exception(e)
+            self.message_user(
+                request,
+                "Probably an error in the url: view it directly. %s" % e)
+        except urllib2.HTTPError, e:
+            logger.exception(e)
+            self.message_user(
+                request,
+                "URL not found? Network error? View url directly. %s" % e)
+
+    reload.short_description = (
+        "Reload WMS connections and update their WMS sources.")
+
+
 admin.site.register(models.WMSSource, WMSSourceAdmin)
-admin.site.register(models.WMSConnection)
+admin.site.register(models.WMSConnection, WMSConnectionAdmin)
