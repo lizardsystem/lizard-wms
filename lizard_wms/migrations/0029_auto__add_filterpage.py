8af6a67d99905ea6a3469c2f06434b0474a6b89d
Reinout van Rees <reinout@vanrees.org>
2013-02-20 13:32:50 +0100

    Added migration for filterpage

---
+++ lizard_wms/migrations/0029_auto__add_filterpage.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'FilterPage'
+        db.create_table('lizard_wms_filterpage', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('wms_source', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['lizard_wms.WMSSource'])),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
+        ))
+        db.send_create_signal('lizard_wms', ['FilterPage'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'FilterPage'
+        db.delete_table('lizard_wms_filterpage')
+
+
+    models = {
+        'lizard_maptree.category': {
+            'Meta': {'ordering': "('index', 'name')", 'object_name': 'Category'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'index': ('django.db.models.fields.IntegerField', [], {'default': '1000'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['lizard_maptree.Category']", 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '20'})
+        },
+        'lizard_wms.featureline': {
+            'Meta': {'object_name': 'FeatureLine'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_hover': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'order_using': ('django.db.models.fields.IntegerField', [], {'default': '1000'}),
+            'render_as': ('django.db.models.fields.CharField', [], {'default': "u'T'", 'max_length': '1'}),
+            'use_as_id': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'visible': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'wms_layer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['lizard_wms.WMSSource']"})
+        },
+        'lizard_wms.filterpage': {
+            'Meta': {'ordering': "(u'wms_source',)", 'object_name': 'FilterPage'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'wms_source': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['lizard_wms.WMSSource']"})
+        },
+        'lizard_wms.wmsconnection': {
+            'Meta': {'object_name': 'WMSConnection'},
+            'category': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['lizard_maptree.Category']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'options': ('django.db.models.fields.TextField', [], {'default': 'u\'{"buffer": 0, "isBaseLayer": false, "opacity": 0.5}\''}),
+            'params': ('django.db.models.fields.TextField', [], {'default': 'u\'{"height": "256", "width": "256", "styles": "", "format": "image/png", "tiled": "true", "transparent": "true"}\''}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'version': ('django.db.models.fields.CharField', [], {'default': "u'1.3.0'", 'max_length': '20'}),
+            'xml': ('django.db.models.fields.TextField', [], {'default': "u''", 'blank': 'True'})
+        },
+        'lizard_wms.wmssource': {
+            'Meta': {'ordering': "(u'index', u'display_name')", 'object_name': 'WMSSource'},
+            '_params': ('jsonfield.fields.JSONField', [], {'null': 'True', 'blank': 'True'}),
+            'bbox': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'category': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['lizard_maptree.Category']", 'null': 'True', 'blank': 'True'}),
+            'connection': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['lizard_wms.WMSConnection']", 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'enable_search': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'index': ('django.db.models.fields.IntegerField', [], {'default': '1000'}),
+            'layer_name': ('django.db.models.fields.TextField', [], {}),
+            'legend_url': ('django.db.models.fields.CharField', [], {'max_length': '2048', 'null': 'True', 'blank': 'True'}),
+            'metadata': ('jsonfield.fields.JSONField', [], {'null': 'True', 'blank': 'True'}),
+            'options': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'show_legend': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['lizard_wms']
