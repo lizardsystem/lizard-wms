lizardsystem/lizard-wms

commit 08277916abb09382a1241aadafa53c9881315fd5

Lizard wms uses layer name as the layer name.
Update in admin, migrations and models.wmsconnection.fetch.

lizard_wms/migrations/0026_move_data_params_to_json_field_new_params.py
@@ -9,11 +9,12 @@ from django.utils import simplejson as json
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        db.alter_column('lizard_wms_wmssource', 'layer_name', self.gf('django.db.models.fields.CharField')(max_length=100))
+        db.alter_column('lizard_wms_wmssource', 'layer_name', self.gf('django.db.models.fields.TextField')())
 
         for wmssource in orm.WMSSource.objects.all():
             params = json.loads(wmssource.params)
             layer_name = params['layers']
+            del params['layers']
             wmssource.new_params = params
             wmssource.layer_name = layer_name
             wmssource.save()

lizard_wms/admin.py
@@ -116,7 +116,7 @@ class WMSSourceAdmin(admin.ModelAdmin):
 
 class SourceInline(admin.TabularInline):
     model = models.WMSSource
-    fields = ('display_name', 'layer_name', '_params', 'options', 'category',
+    fields = ('display_name', 'layer_name', 'options', 'category',
               'index')
     extra = 0
     formfield_overrides = {

lizard_wms/models.py
@@ -102,7 +102,7 @@ class WMSConnection(models.Model):
             u"because owslib can only handle 1.1.1."))
 
     params = models.TextField(
-        default='{"height": "256", "width": "256", "layers": "%s", '
+        default='{"height": "256", "width": "256", '
         '"styles": "", "format": "image/png", "tiled": "true", '
         '"transparent": "true"}')
     options = models.TextField(
@@ -159,7 +159,7 @@ overwrites.""")
                 layer_instance.url = self.url
                 layer_instance.options = self.options
                 layer_instance.category = self.category.all()
-                layer_instance._params = self.params % layer.name
+                layer_instance._params = self.params
                 layer_instance.import_bounding_box(layer)
             except:
                 logger.exception("Something went wrong. We skip this layer")
@@ -193,7 +193,7 @@ class WMSSource(models.Model):
     Definition of a wms source.
     """
 
-    layer_name = models.CharField(max_length=100)
+    layer_name = models.TextField()
     display_name = models.CharField(max_length=255, null=True, blank=True)
     url = models.URLField(verify_exists=False)
     _params = JSONField(null=True, blank=True)

commit 2596f65fb1b9489c687f3e346b4b7cadcc213d5e

Fixed migrations.
Fixes problems introduced by 08277916abb09382a1241aadafa53c9881315fd5

lizard_wms/migrations/0026_move_data_params_to_json_field_new_params.py
@@ -74,7 +74,7 @@ class Migration(SchemaMigration):
             'enable_search': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'index': ('django.db.models.fields.IntegerField', [], {'default': '1000'}),
-            'layer_name': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'layer_name': ('django.db.models.fields.TextField', []),
             'legend_url': ('django.db.models.fields.CharField', [], {'max_length': '2048', 'null': 'True', 'blank': 'True'}),
             'metadata': ('jsonfield.fields.JSONField', [], {'null': 'True', 'blank': 'True'}),
             'new_params': ('jsonfield.fields.JSONField', [], {'null': 'True', 'blank': 'True'}),

lizard_wms/migrations/0027_auto__del_field_wmssource_params.py
@@ -63,7 +63,7 @@ class Migration(SchemaMigration):
             'enable_search': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'index': ('django.db.models.fields.IntegerField', [], {'default': '1000'}),
-            'layer_name': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'layer_name': ('django.db.models.fields.TextField', []),
             'legend_url': ('django.db.models.fields.CharField', [], {'max_length': '2048', 'null': 'True', 'blank': 'True'}),
             'metadata': ('jsonfield.fields.JSONField', [], {'null': 'True', 'blank': 'True'}),
             'new_params': ('jsonfield.fields.JSONField', [], {'null': 'True', 'blank': 'True'}),

lizard_wms/migrations/0028_auto__del_field_wmssource_new_params__add_field_wmssource__params.py
@@ -60,7 +60,7 @@ class Migration(SchemaMigration):
             'enable_search': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'index': ('django.db.models.fields.IntegerField', [], {'default': '1000'}),
-            'layer_name': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'layer_name': ('django.db.models.fields.TextField', []),
             'legend_url': ('django.db.models.fields.CharField', [], {'max_length': '2048', 'null': 'True', 'blank': 'True'}),
             'metadata': ('jsonfield.fields.JSONField', [], {'null': 'True', 'blank': 'True'}),
             'options': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),