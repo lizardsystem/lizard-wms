584596fe943c73bb7d5a42c8bb8660efe96087fd
Reinout van Rees <reinout@vanrees.org>
2013-02-27 14:17:19 +0100

    Refactored the refresh part into a separate function.
    (This way we can call it later on from a select-has-changed event).

--- lizard_wms/static/lizard_wms/filter_page.js
+++ lizard_wms/static/lizard_wms/filter_page.js
@@ -1,30 +1,34 @@
-function setUpFilterPageForm() {
-  var $form = $("#filter-page-form");
-  $form.submit(function(e) {
-    e.preventDefault();
-    return $.ajax({
-        url: $form.attr('action'),
-        type: "GET",
-        data: $form.serialize(),
-        success: function(data) {
-          $("#lizard-map-wms").html($(data).find(
-            "#lizard-map-wms").html());
-          refreshWmsLayers();
-          $("#filter-page-form fieldset").html($(data).find(
-            "#filter-page-form fieldset").html());
-          $("#filter-page-download-button").html($(data).find(
-            "#filter-page-download-button").html());
-          $(".workspace-wms-layer").each(function () {
-            var id = $(this).attr("data-workspace-wms-id");
-            var params = $(this).attr("data-workspace-wms-params");
-            params = $.parseJSON(params);
-            wms_layers[id].mergeNewParams(params);
-          });
-        }
-    });
+function refreshFormAndLayers() {
+  return $.ajax({
+    url: $("#filter-page-form").attr('action'),
+    type: "GET",
+    data: $("#filter-page-form").serialize(),
+    success: function(data) {
+      $("#lizard-map-wms").html($(data).find(
+        "#lizard-map-wms").html());
+      refreshWmsLayers();
+      $("#filter-page-form fieldset").html($(data).find(
+        "#filter-page-form fieldset").html());
+      $("#filter-page-download-button").html($(data).find(
+        "#filter-page-download-button").html());
+      $(".workspace-wms-layer").each(function () {
+        var id = $(this).attr("data-workspace-wms-id");
+        var params = $(this).attr("data-workspace-wms-params");
+        params = $.parseJSON(params);
+        wms_layers[id].mergeNewParams(params);
+      });
+    }
+  });
+}
 
+
+function setUpFilterPageForm() {
+  $("#filter-page-form").submit(function(e) {
+    e.preventDefault();
+    return refreshFormAndLayers();
   })
 }
+
 
 $(document).ready(function () {
     setUpFilterPageForm();
