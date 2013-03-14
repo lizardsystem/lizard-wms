c4251ec911951ff60b693cedb36719eb8bee24b5
Reinout van Rees <reinout@vanrees.org>
2013-03-14 15:32:12 +0100

    Added functional test that renders the popup template.
    This way I can test whether the mark_safe() machinery works.

--- lizard_wms/tests/test_feature_line.py
+++ lizard_wms/tests/test_feature_line.py
@@ -1,8 +1,9 @@
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
 
+from django.template import Context
 from django.test import TestCase
-# from django.utils import simplejson as json
+from django.template.loader import get_template
 
 from lizard_wms.tests import factories
 from lizard_wms import popup_renderers
@@ -35,3 +36,19 @@ class FeatureLineTest(TestCase):
         expected2 = 'Click here'
         self.assertTrue(expected1 in result['value'])
         self.assertTrue(expected2 in result['value'])
+
+    def test_functional(self):
+        # Bare-bones template test to check if mark_safe works properly.
+        template = get_template('lizard_wms/popup.html')
+        self.feature_line.render_as = popup_renderers.RENDER_URL
+        result1 = self.feature_line.as_popup_info(
+            'http://reinout.vanrees.org')
+        self.feature_line.render_as = popup_renderers.RENDER_IMAGE
+        result2 = self.feature_line.as_popup_info(
+            'http://reinout.vanrees.org/logo.png')
+        context = Context({'feature_info': [result1, result2]})
+        output = template.render(context)
+        expected1 = '<img src'
+        expected2 = '<a href'
+        self.assertTrue(expected1 in output)
+        self.assertTrue(expected2 in output)
