c6d531385c1762a7d5bde39567abb486efb097bf
Reinout van Rees <reinout@vanrees.org>
2014-03-18 16:37:09 +0100

    Trying a local logger

--- lizard_wms/models.py
+++ lizard_wms/models.py
@@ -533,7 +533,8 @@ like {"key": "value", "key2": "value2"}.
         autharg = {}
         if proxy:
             autharg = {'auth': (proxy['username'], proxy['password'])}
-        logger.error(self.params)
+        locallogger = logging.getLogger(__name__)
+        locallogger.error(self.params)
         for layer in params['layers'].split(","):
             payload = self._build_payload(params, layer, feature_count,
                                           version, bbox, width, height, x, y,
