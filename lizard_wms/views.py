fb0b702a73054bb43601c249f3f2e5a8e866c1c4
Reinout van Rees <reinout@vanrees.org>
2014-03-31 12:17:12 +0200

    Added max-age cache control header

--- lizard_wms/views.py
+++ lizard_wms/views.py
@@ -202,7 +202,8 @@ class TimeWmsView(MapView):
     """View for a wms layer with TIME parameter."""
     template_name = 'lizard_wms/time.html'
 
-    @method_decorator(cache_control(private=True))
+    @method_decorator(cache_control(private=True,
+                                    max_age=5 * 60))
     def dispatch(self, *args, **kwargs):
         return super(TimeWmsView, self).dispatch(*args, **kwargs)
 
