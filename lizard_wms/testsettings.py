ef290c119550c958f5e1e4889d70ee9fe3953cc7
Remco Gerlich <remco@gerlich.nl>
2014-09-02 14:58:54 +0200

    Add SECRET_KEY

--- lizard_wms/testsettings.py
+++ lizard_wms/testsettings.py
@@ -128,6 +128,8 @@ SOUTH_TESTS_MIGRATE = False
 
 LIZARD_WMS_STANDALONE = True
 
+SECRET_KEY = "Doesn't need to be secret in testsettings."
+
 try:
     # Import local settings that aren't stored in svn.
     from lizard_wms.local_testsettings import *
